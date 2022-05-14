e30fcd7ea96bf06846adaad70ce4be8dfb27a688
Akito Koga <akito121215@gmail.com>
2022-05-14 10:07:44 +0900

    Django管理画面を最適化

--- src/backend/app/settings.py
+++ src/backend/app/settings.py
@@ -23,6 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(DEBUG=(bool, False))
 env.read_env(os.path.join(BASE_DIR, '.env'))
 
+# Static Files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
