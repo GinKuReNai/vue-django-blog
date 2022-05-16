5be00e498dd4e75c01d7ef2df2fc92bd35411c26
GinKuReNai <akito121215@gmail.com>
2022-05-17 01:21:06 +0900

    Git pull時のVueの不具合を修正

--- docker/vue/docker-entrypoint.sh
+++ docker/vue/docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# Package依存関係の修正
+echo "Fix Package Dependencies"
+npm audit fix
+
 # Collect static files
-echo "Build Vue Component"
-npm run build
+# echo "Build Vue Component"
+# npm run build
 
 # Start server
 echo "Starting server"
