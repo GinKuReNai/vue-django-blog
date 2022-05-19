e343da338328608fc38b173d5461e00235d86f33
GinKuReNai <akito121215@gmail.com>
2022-05-19 18:04:43 +0900

    Headerを追加

---
+++ src/frontend/src/components/GlobalHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header-container">
+    <nav class="header">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
